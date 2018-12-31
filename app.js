91465e421b746fbc2bfe6d3a40bda40780e859a8
Fahad H <hoss.fahad@gmail.com>
2018-12-31 02:30:31 -0500

    added dashboard route + connected to mongodb + beginning styles of view

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//mongodb and connection
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
@@ -11,6 +17,9 @@ app.set('view engine', 'ejs');
 // Routes 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+app.use('/dashboard', require('./routes/dashboard'));
+
+app.use(express.static(__dirname + '/public'));
 
 // app.use('/', require('./routes/index'));
 const PORT = process.env.PORT || 5000;
