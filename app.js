e76b351d0163abdf46ee5dbd34abf9ee84b18e6f
Fahad H <hoss.fahad@gmail.com>
2019-01-03 01:37:09 -0500

    mongoose schema, prettified, field and pw logic added

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 
 //mongodb and connection
 const db = require('./config/keys').mongoURI;
-mongoose.connect(db, { useNewUrlParser: true })
+mongoose.connect(db, {
+        useNewUrlParser: true
+    })
     .then(console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
@@ -14,11 +16,16 @@ mongoose.connect(db, { useNewUrlParser: true })
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+//Body Parser 
+app.use(express.urlencoded({
+    extended: false
+}));
+
 // Routes 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 app.use('/dashboard', require('./routes/dashboard'));
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on ${PORT}`));
-
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
-
+// Login and Register Page
 router.get('/login', (req, res) => res.render('login'));
 router.get('/register', (req, res) => res.render('register'));
+
+// Registration Data
+router.post('/register', (req, res) => {
+    const {
+        name,
+        email,
+        password,
+        password2
+    } = req.body;
+    let errors = [];
+
+    // Form requirements
+    if (!name || !email || !password || !password2) {
+        errors.push({
+            msg: 'Please fill in the required fields'
+        })
+    } else {
+        res.render('login');
+        console.log('User succesfully registered');
+    };
+    // Password requirements
+    if (password !== password2) {
+        error.push({
+            msg: 'Passwords do not match'
+        })
+        return false;
+    } else {
+        return true;
+    }
+});
 
 module.exports = router;
